b85d214e9fe28370543fd2eef04cf2307ced7edd
Ryan <ryanathomas013@gmail.com>
2022-12-25 19:36:23 +0000

    saving checks throughout reload

--- Hypixel RNG List/main.js
+++ Hypixel RNG List/main.js
@@ -1,32 +1,53 @@
-function storeCookie() {
-  // Get all the checkboxes
-  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const img = document.querySelector("img");
+img.setAttribute("draggable", false);
 
-  // Create an object to store the values of the checked checkboxes
-  var checkedValues = {};
+var checked = {};
 
-  // Loop through the checkboxes and store the values of the checked ones in the object
-  for (var i = 0; i < checkboxes.length; i++) {
-    if (checkboxes[i].checked) {
-      checkedValues[checkboxes[i].name] = checkboxes[i].value;
-    }
+function highlight() {
+  if (event.target.parentElement.closest("div").classList.contains("checked")) {
+    event.target.parentElement.closest("div").classList.remove("checked");
+    updatedStorage(false);
+  } else {
+    updatedStorage(true);
+    event.target.parentElement.closest("div").classList.add("checked");
   }
-
-  // Convert the object to a string and store it in a cookie
-  document.cookie = "checkedValues=" + JSON.stringify(checkedValues);
+}
+function highlightOnLoad(name) {
+  document.querySelector("." + name).classList.add("checked");
+}
+function updatedStorage(added) {
+  if (added) {
+    checked[event.target.parentElement.closest("div").className] = {
+      checked: true,
+    };
+    setStorage();
+  } else {
+    delete checked[event.target.parentElement.closest("div").className];
+    setStorage();
+  }
 }
 
-// When the page loads, check the checkboxes based on the values stored in the cookie
-window.addEventListener("load", function () {
-  // Get the stored cookie string
-  var cookieString = document.cookie;
+function setStorage() {
+  let checked_serialized = JSON.stringify(checked);
+  localStorage.setItem("checked", checked_serialized);
+}
 
-  // If the cookie string is not empty, convert it back to an object and use it to check the appropriate checkboxes
-  if (cookieString) {
-    var checkedValues = JSON.parse(cookieString.split("=")[1]);
+function getChecked() {
+  return JSON.parse(localStorage.getItem("checked"));
+}
 
-    for (var key in checkedValues) {
-      document.getElementById(key).checked = true;
-    }
+function checkedRefresh() {
+  checked = getChecked();
+
+  for (let [name, info] of Object.entries(checked)) {
+    highlightOnLoad(name);
   }
+}
+
+window.addEventListener("beforeunload", () => {
+  localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.documentElement.scrollTop = localStorage.getItem("sidebar-scroll");
 });
